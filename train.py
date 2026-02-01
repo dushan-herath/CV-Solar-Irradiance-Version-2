5e440475eef0d4f07413af01d4ebe55a13f0c0fa
dushan-herath <dushan.herath123@icloud.com>
2026-01-31 18:57:40 -0600

    Update

--- train.py
+++ train.py
@@ -167,7 +167,7 @@ if __name__ == "__main__":
         img_size=224,
         patch_size=4,
         embed_dim=128,
-        depth=8,
+        depth=4,
         num_heads=16,
         dropout=0.3
     )
